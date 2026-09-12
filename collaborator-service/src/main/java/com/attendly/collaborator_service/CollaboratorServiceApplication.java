f462bacc56f705c58d6ab9b900327ad62bdee301
fh3mrique <fhemrique55@gmail.com>
2026-09-12 19:52:27 -0300

    collaborator-service created

---
+++ collaborator-service/src/main/java/com/attendly/collaborator_service/CollaboratorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.attendly.collaborator_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaboratorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaboratorServiceApplication.class, args);
+	}
+
+}
---
+++ collaborator-service/src/test/java/com/attendly/collaborator_service/CollaboratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendly.collaborator_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaboratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
